49cffa4dd0add16dcf2cc695575e7ef3bc36a28f
Alexander Grachev <awgrachev@gmail.com>
2024-05-19 11:18:06 -0700

    remove base url from config file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    "baseUrl": "https://gymdesk.com/login",
     "viewportWidth": 1440,
     "viewportHeight": 900,
   },
