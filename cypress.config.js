3a69477e22f6489d92a0a5ae745cff01632af290
Alexander Grachev <awgrachev@gmail.com>
2024-05-19 11:12:27 -0700

    npm modules update

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    "baseUrl": "https://gymdesk.com/login",
+    "viewportWidth": 1440,
+    "viewportHeight": 900,
   },
 });
