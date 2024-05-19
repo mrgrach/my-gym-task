a14a2ed423f0003379c22c533ec0036d77c4d0e8
Alexander Grachev <awgrachev@gmail.com>
2024-05-19 13:36:46 -0700

    Finish a test scenario with adding a product

--- cypress/e2e/addProduct.cy.js
+++ cypress/e2e/addProduct.cy.js
@@ -1,4 +1,12 @@
 import SigninPage from "../pages/SigninPage.js";
+import NavigationPage from "../pages/NavigationPage.js";
+
+// const faker = require('faker');
+const { faker } = require('@faker-js/faker');
+const randomProductName = faker.commerce.product() + 'drink';
+const randomPrice = faker.finance.amount({ min: 4, max: 15 });
+const randomInventorySku = faker.number.int({ min: 10, max: 10000 },
+
 
 context('As a gym owner user', () => {
     before(() => {
@@ -6,14 +14,38 @@ context('As a gym owner user', () => {
       cy.clearCookies()
     });
     
-    describe('After succesfull login', () => {
-      it('Able to add a product', () => {
+    describe('Able to add a product with online shop disabled', () => {
+    it('Sign in', () => {
         cy.fixture('users.json').then((users) => {
-          const user = users[0]
-          SigninPage.login(users.email, users.password)
-          cy.url().should('eq', 'https://gymdesk.com/manager/dashboard')
-          cy.wait(3000)
+            const user = users[0]
+            SigninPage.login(users.email, users.password)
         });
-    })
-})});
+        cy.url().should('eq', 'https://gymdesk.com/manager/dashboard')
+    });
 
+    it('Navigate to Sales', () => {
+        NavigationPage.clickOnSalesOnLeftNavBar()
+        cy.url().should('eq', 'https://gymdesk.com/manager/sales/create')
+    });
+
+    it('Navigate to Add Product', () => {
+        NavigationPage.clickOnProductsOnTopNavBar()
+        cy.url().should('eq', 'https://gymdesk.com/manager/products/list')
+    });
+
+    it('Initiate Adding a Product', () => {
+        NavigationPage.clickOnAddProductButton()
+        cy.url().should('eq', 'https://gymdesk.com/manager/products/add')
+    })
+
+    it('It fills out a form and save a product', () => {
+        NavigationPage.enterProductNameInput(randomProductName)
+        NavigationPage.selectCategory()
+        NavigationPage.enterPrice(randomPrice)
+        NavigationPage.enterInventorySku(randomInventorySku)
+        NavigationPage.clickOnSaveProductButton()
+        cy.url().should('eq', 'https://gymdesk.com/manager/products/list')
+        cy.contains(randomProductName)
+    })
+}); 
+}));
